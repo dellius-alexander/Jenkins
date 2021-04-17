d8ac8210cd21daad5c374fe3c1c5dee3152f3246
dellius-alexander <dellius.alexander@gmail.com>
2021-04-17 00:00:38 -0400

    updated

--- init_.sh
+++ init_.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 ################################################################
 ################################################################
+if [[ $1 =~ ^(start)$ ]]; then
+export $(cat $(find -type f -name 'jenkins.env') | grep -v '#' | awk '/=/ {print $1}')
 kubectl apply -f $(find -type f -name 'jenkins-rbac.yaml')
 kubectl apply -f $(find -type f -name 'jenkins-volume.yaml')
 kubectl apply -f $(find -type f -name 'private-docker-registry.yaml')
 kubectl apply -f $(find -type f -name 'jenkins-deployment.yaml')
+elif [[ $1 =~ ^(stop)$ ]]; then
+export $(cat $(find -type f -name 'jenkins.env') | grep -v '#' | awk '/=/ {print $1}')
+kubectl delete -f $(find -type f -name 'jenkins-deployment.yaml')
+kubectl delete -f $(find -type f -name 'jenkins-volume.yaml')
+kubectl delete -f $(find -type f -name 'jenkins-rbac.yaml')
+fi
+
